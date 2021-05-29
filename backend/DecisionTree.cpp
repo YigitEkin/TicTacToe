69f64947013367e9c8765b05804043229bdf3eda
Harun <can.surav@ug.bilkent.edu.tr>
2021-05-29 18:00:34 +0300

    Decision Tree is created.

---
+++ backend/DecisionTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Harun Can on 5/29/2021.
+//
+
+#include "DecisionTree.h"
---
+++ backend/DecisionTree.h
@@ -0,0 +1,20 @@
+//
+// Created by Harun Can on 5/29/2021.
+//
+
+#ifndef BACKEND_DECISIONTREE_H
+#define BACKEND_DECISIONTREE_H
+#include "nWayNode.h"
+
+class DecisionTree {
+public:
+	DecisionTree();
+	~DecisionTree();
+
+private:
+	nWayNode* root;
+
+};
+
+
+#endif //BACKEND_DECISIONTREE_H
